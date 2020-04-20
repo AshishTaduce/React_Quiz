ddf26bfedc7a4945701fb2f82947c710817bc1c4
AshishTaduce <ashishteddy2@gmail.com>
2020-04-20 12:02:57 +0530

    Commit 2

---
+++ precache-manifest.cc1bc80fe16d99c80123f5d0d7899a7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "667ee3d3087b0af6db00a31c460fa9b1",
+    "url": "/React_Quiz/index.html"
+  },
+  {
+    "revision": "5f0bbface4596c7dfc84",
+    "url": "/React_Quiz/static/css/main.9fae68fa.chunk.css"
+  },
+  {
+    "revision": "041ff7497011f4b3b6cd",
+    "url": "/React_Quiz/static/js/2.d19dfa62.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Quiz/static/js/2.d19dfa62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0bbface4596c7dfc84",
+    "url": "/React_Quiz/static/js/main.5cab0b1d.chunk.js"
+  },
+  {
+    "revision": "00096f2e27418c3a2f3b",
+    "url": "/React_Quiz/static/js/runtime-main.196d746e.js"
+  }
+]);
